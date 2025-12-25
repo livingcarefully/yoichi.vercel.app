c5605aa149b7c72e26542bbfa713f631f1c2221a
Yoichi <codexyy01@gmail.com>
2025-12-25 13:00:50 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 document.addEventListener('mousemove', (e) => {
-  const parallaxElement = document.querySelector('.parallax');
-  if(parallaxElement){
-    const x = (window.innerWidth/2 - e.clientX) / 40;
-    const y = (window.innerHeight/2 - e.clientY) / 40;
-    parallaxElement.style.transform = `translate(${x}px, ${y}px)`;
-  }
+  const body = document.body;
+
+  const x = (window.innerWidth / 2 - e.clientX) / 40;
+  const y = (window.innerHeight / 2 - e.clientY) / 40;
+
+  body.style.backgroundPosition = `calc(50% + ${x}px) calc(50% + ${y}px)`;
 });
